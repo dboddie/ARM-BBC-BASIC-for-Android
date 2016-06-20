01684c5bf6519a34701192bfa819c25451049b09
David Boddie <david@boddie.org.uk>
2016-06-20 16:35:37 +0200

    Added notes to the build script.

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# Change the following line to point to the correct location of the cross-
+# building toolchain on your system.
 SYSROOT=/home/build/Android/android-ndk-r9d/platforms/android-15/arch-arm
+# Change the following line to point to the location of the asasm binary.
+# See http://www.riscos.info/index.php/Using_GCCSDK for details of how to
+# build the toolchain that contains it.
+ASASM=../asasm
 
-../asasm -From BASIC105.s basic.elf
+$ASASM -From BASIC105.s basic.elf
 arm-linux-androideabi-gcc -c -o main.o main.c --sysroot=$SYSROOT
 arm-linux-androideabi-gcc -o basic main.o basic.elf --sysroot=$SYSROOT
